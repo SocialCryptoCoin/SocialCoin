23b2b993bfb37755d0a7717d2f9fc83d47a84138
Matt Corallo <coingenbot@bluematt.me>
2014-01-03 11:41:09 +0100

    Change network magic

--- src/main.cpp
+++ src/main.cpp
@@ -3057,7 +3057,7 @@ bool static AlreadyHave(const CInv& inv)
 // The message start string is designed to be unlikely to occur in normal data.
 // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
 // a large 4-byte int at any alignment.
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0x32, 0xf1, 0x24, 0x23 };
 
 
 void static ProcessGetData(CNode* pfrom)
