026679f0483e5bc4d008c9420243bce6591e6c0f
root <root@ubuntu.ubuntu>
2014-01-05 05:51:47 -0500

    Fixed targeting

--- src/main.cpp
+++ src/main.cpp
@@ -1081,8 +1081,8 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
     return nSubsidy + nFees;
 }
 
-static const int64 nTargetTimespan = 14 * 24 * 60 * 60; // two weeks
-static const int64 nTargetSpacing = 10 * 60;
+static const int64 nTargetTimespan = 30 * 60; // 30 minutes
+static const int64 nTargetSpacing = 60; // 1 minute
 static const int64 nInterval = nTargetTimespan / nTargetSpacing;
 
 //
